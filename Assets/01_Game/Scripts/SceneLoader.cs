b0e3d92ca81600cf866342c2d5b928e96cbee50c
Kirato Kumakura <u9b3d5kuesehbns6s6h7@docomo.ne.jp>
2025-12-04 10:08:45 +0900

    シーン遷移ツール追加

---
+++ Assets/01_Game/Scripts/SceneLoader.cs
@@ -0,0 +1,85 @@
+using EasyTransition;
+using UnityEngine;
+using R3;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // ---------- SerializeField
+    [SerializeField] private TransitionManager _transitionManager;
+    [SerializeField] private TransitionSettings _transitionSettings;
+
+    // ---------- Property
+    public bool IsRunning => _transitionManager.IsRunning;
+
+    // ---------- R3
+    public Observable<Unit> OnTransitionStarted => _transitionManager.OnTransitionStarted;
+    public Observable<Unit> OnTransitionHalf => _transitionManager.OnTransitionHalf;
+    public Observable<Unit> OnTransitionCompleted => _transitionManager.OnTransitionCompleted;
+
+    // ---------- Method
+    /// <summary>
+    /// 現在のシーンをロード
+    /// </summary>
+    public void ReloadScene()
+    {
+        if (IsRunning) return;
+
+        _transitionManager.Transition(
+            SceneManager.GetActiveScene().name,
+            _transitionSettings);
+    }
+
+    public void ReloadScene(float startDelay)
+    {
+        if (IsRunning) return;
+
+        _transitionManager.Transition(
+            SceneManager.GetActiveScene().name,
+            _transitionSettings,
+            startDelay);
+    }
+
+    /// <summary>
+    /// 対象のシーンへ遷移
+    /// </summary>
+    /// <param name="sceneName">シーン名</param>
+    public void LoadScene(
+       string sceneName)
+    {
+        if (IsRunning) return;
+
+        _transitionManager.Transition(sceneName, _transitionSettings);
+    }
+
+    public void LoadScene(
+        int sceneIndex)
+    {
+        if (IsRunning) return;
+
+        _transitionManager.Transition(sceneIndex, _transitionSettings);
+    }
+
+    /// <summary>
+    /// 対象のシーンへ遷移(開始遅延あり)
+    /// </summary>
+    /// <param name="sceneName">シーン名</param>
+    /// <param name="startDelay">遅延</param>
+    public void LoadScene(
+        string sceneName,
+        float startDelay)
+    {
+        if (IsRunning) return;
+
+        _transitionManager.Transition(sceneName, _transitionSettings, startDelay);
+    }
+
+    public void LoadScene(
+        int sceneIndex,
+        float startDelay)
+    {
+        if (IsRunning) return;
+
+        _transitionManager.Transition(sceneIndex, _transitionSettings, startDelay);
+    }
+}
