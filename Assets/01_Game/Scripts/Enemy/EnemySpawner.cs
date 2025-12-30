e5d64f5dd8371b4b32a1cc49d9bdd9c3698c868f
Kirato Kumakura <kss-23130076@nsgcl.jp>
2025-12-31 03:09:10 +0900

    エネミーのスポーンシステム作成

---
+++ Assets/01_Game/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // ---------- SerializeField
+    [SerializeField] private List<Transform> _spawnTransfroms = new();
+
+    // ---------- UnityMessage
+    private void Start()
+    {
+        var enemyManager = EnemyManager.Instance;
+
+        var enemyDataStore = FindAnyObjectByType<EnemyDataStore>();
+
+        foreach(var tr in _spawnTransfroms)
+        {
+            var enemyId = Random.Range(0, enemyDataStore.GetCount);
+
+            var targetEnemy = enemyDataStore.FindWithId(enemyId).EnemyPrefab;
+
+            var clone = Instantiate(
+                targetEnemy,
+                tr.position,
+                Quaternion.identity,
+                enemyManager.transform);
+
+            enemyManager.AddEnemy(clone);
+        }
+    }
+
+    // ---------- Method
+    [ContextMenu("AddChildrenIntoList")]
+    public void GetChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (_spawnTransfroms.Contains(transform.GetChild(i))) continue;
+
+            _spawnTransfroms.Add(transform.GetChild(i));
+        }
+    }
+}
